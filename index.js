97aec1134b9f36db1ee018615edf29ccf36891f2
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-10-02 01:36:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,60 +1,44 @@
-/* Your Code Here */
-
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
-let createEmployeeRecord = function (x){
-  let timeIn = [];
-  let timeOut = [];
-  let obj = {
-    firstName: x[0],
-    familyName: x[1],
-    title: x[2],
-    payPerHour: x[3],
-    timeInEvents: timeIn,
-    timeOutEvents: timeOut
-  }
-  return obj;
+let createEmployeeRecord = function(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
 }
-let createEmployeeRecords = function (x){
 
-  let newArray = x.map(createEmployeeRecord);
-  return newArray;
+let createEmployeeRecords = function(employeeRowData) {
+    return employeeRowData.map(function(row){
+        return createEmployeeRecord(row)
+    })
 }
-let createTimeInEvent = function (y){
-  let z = Array.from(y);
-  let date1 = z.slice(0,10);
-  date1 = date1.join("");
-  let hour1 = z.slice(11,15);
-  hour1 = parseInt(hour1.join(""),10);
-  let obj2 = {
-    type: "TimeIn",
-    hour: hour1,
-    date: date1
-  };
-  this.timeInEvents.push(obj2)
-return this;
+
+let createTimeInEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
 }
-function createTimeOutEvent(y){
-  let z = Array.from(y);
-  let date1 = z.slice(0,10);
-  date1 = date1.join("");
-  let hour1 = z.slice(11,15);
-  hour1 = parseInt(hour1.join(""),10);
-  let obj2 = {
-    type: "TimeOut",
-    hour: hour1,
-    date: date1
-  }
-  this.timeOutEvents.push(obj2);
-return this;
+
+let createTimeOutEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
 }
+
 let hoursWorkedOnDate = function(soughtDate){
     let inEvent = this.timeInEvents.find(function(e){
         return e.date === soughtDate
@@ -67,29 +51,32 @@ let hoursWorkedOnDate = function(soughtDate){
     return (outEvent.hour - inEvent.hour) / 100
 }
 
-function wagesEarnedOnDate(b){
-  let z=0;
- z = hoursWorkedOnDate.call(this,b) * this.payPerHour;
- return z;
+let wagesEarnedOnDate = function(dateSought){
+    let rawWage = hoursWorkedOnDate.call(this, dateSought)
+        * this.payPerHour
+    return parseFloat(rawWage.toString())
 }
 
-let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
+let allWagesFor = function(){
+    let eligibleDates = this.timeInEvents.map(function(e){
         return e.date
     })
 
-    let payable = eligibleDates.reduce(function (memo, d) {
+    let payable = eligibleDates.reduce(function(memo, d){
         return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+    }.bind(this), 0)
 
     return payable
 }
-function findEmployeeByFirstName(i,j){
-return  i.find(ele => ele.firstName === j)
 
+let findEmployeeByFirstName = function(srcArray, firstName) {
+  return srcArray.find(function(rec){
+    return rec.firstName === firstName
+  })
 }
 
-function calculatePayroll(v){
-   let z = v.reduce(function (accum, sum) {accum + allWagesFor(sum)},0)
-return z;
+let calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
 }
